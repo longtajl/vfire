e27c30468e1112c8e18a95802377c998388cea7a
ngo <shota.nagao@kddi-cf.com>
2018-09-14 19:21:12 +0900

    aa

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyAdtiBxcFZaoARYcM0-GwXtFa5BDkNJv7s",
-    authDomain: "training-a0bb4.firebaseapp.com",
-    databaseURL: "https://training-a0bb4.firebaseio.com",
-    projectId: "training-a0bb4",
-    storageBucket: "training-a0bb4.appspot.com",
-    messagingSenderId: "763657503514"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   })
 }
 
